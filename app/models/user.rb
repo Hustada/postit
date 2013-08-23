6b748a39faa0ed0f1ade272fd816643c9a8e2ff5
Mark <hustada80@gmail.com>
2013-08-23 00:24:04 -0500

    modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
-end
+
+  has_secure_password validations: false
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/register', to: 'users#new'
+
+  resources :users, only: [:create]
+
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
     
   resources :categories, only: [:new, :create]
-
 end
